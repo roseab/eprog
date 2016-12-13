6bbc5643f507bb149b04a10af01ad4ce74c87d8a
Rose Anne D. Bianzon <rabianzon@gmail.com>
2016-12-13 08:57:41 +0800

    Create Phytagorean_theorem.c

---
+++ Phytagorean_theorem.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+
+ main(){
+	
+	/*calculate the c*/
+	int a;
+	int b;
+	int c;
+	
+	
+	
+	printf("Enter the value of a:");
+	scanf("%d", &a);
+	
+	printf("Enter the value of b:");
+	scanf("%d", &b);
+	
+	printf("%d", c = sqrt((a*a)+(b*b)));
+
+	
+	
+	
+}
