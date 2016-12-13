6d53cbb29a0a43c3928fd71114a22363514942f8
Rose Anne D. Bianzon <rabianzon@gmail.com>
2016-12-13 09:09:13 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+
+main(){
+	
+	int num1, num2, num3, average;
+	
+	printf("Enter Number 1:");
+	scanf("%d", &num1);
+	
+	printf("Enter Number 2:");
+	scanf("%d", &num2);
+	
+	printf("Enter Number 3:");
+	scanf("%d", &num3);
+	
+	printf("%d", average = ((num1+num2+num3)/3));
+}
