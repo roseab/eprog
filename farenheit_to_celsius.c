7e7f63157dbcaee40786130aa043960b41f3aca9
Rose Anne D. Bianzon <rabianzon@gmail.com>
2016-12-16 23:33:39 +0800

    Create farenheit_to_celsius.c

---
+++ farenheit_to_celsius.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+  float c;
+  float f;
+
+  printf("Enter degree Farenheit:");
+  scanf("%lf", &f)
+
+  c = (5/9)*(f-32)
+  printf("Celsius =");
+
+}
