4578b997c52de37d22c9be54d0b56efd1f69f769
Rose Anne D. Bianzon <rabianzon@gmail.com>
2016-12-20 08:19:11 +0800

    Update if_play.c

--- if_play.c
+++ if_play.c
@@ -1,15 +1,17 @@
-#include<stdio.h>
+
+#include <stdio.h>
+
 int main(){
 	int num;
 	
 	printf("Enter Num:");
 	scanf("%d", &num);
-	if(num%3 == 0){
+	if(num%5 == 0){
 		printf("Ed");
 	}
-	if(num%5 == 0){
+	if(num%3 == 0){
 		printf("Pogi");
-	}	
+	}
 	if(num%3,5 == 0){
 		printf("Ed Pogi");
 	}
